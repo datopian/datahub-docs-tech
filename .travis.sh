5d184206a8c76dff357f7015127a84e2a25b88c5
subhankar <subhankar.biswas@target.com>
2017-02-17 07:53:42 +0530

    [s] Update travis build script

--- .travis.sh
+++ .travis.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 MKDOCS_SITE="${HOME}/mkdocs_site"
 mkdir $MKDOCS_SITE
-git clone -b gh-pages "https://github.com/${TRAVIS_REPO_SLUG}" $MKDOCS_SITE
+git clone -b gh-pages "https://$GH_TOKEN@github.com/frictionlessdata/dpr-docs.git" $MKDOCS_SITE
 mkdocs build -d $MKDOCS_SITE
 cd $MKDOCS_SITE
-git remote set-url origin "https://$GH_TOKEN@github.com/${TRAVIS_REPO_SLUG}.git"
 git add *
 git commit -m "Deployed from travis"
 git push origin gh-pages
