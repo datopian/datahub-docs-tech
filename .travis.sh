5aa61e323f65f645468e7e53e82a7ba237d25613
subhankar <subhankar.biswas@target.com>
2017-02-17 08:00:44 +0530

    [s] Update travis build script with git user name and email

--- .travis.sh
+++ .travis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 MKDOCS_SITE="${HOME}/mkdocs_site"
 mkdir $MKDOCS_SITE
+git config --global user.email $GH_EMAIL
+git config --global user.name $GH_NAME
 git clone -b gh-pages "https://$GH_TOKEN@github.com/frictionlessdata/dpr-docs.git" $MKDOCS_SITE
 mkdocs build -d $MKDOCS_SITE
 cd $MKDOCS_SITE
